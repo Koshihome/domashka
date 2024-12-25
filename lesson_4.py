3d69ca9691ee9e7857271369b0579ca037fc8d24
Лика <angely35.nk@gmail.com>
2024-12-25 05:19:06 +0300

    lesson_4.py

--- lesson_4.py
+++ lesson_4.py
@@ -4,3 +4,13 @@ hehe = input()
 nothehe = hehe.replace('н', '!')
 print(nothehe)
 print (len(max(re.findall(r'н+', hehe))))
+
+# 2 задание
+import re
+hehe = input()
+print(re.findall(r'\((.+?)\)', hehe))
+
+# 3 задание
+import re
+hehe = input().lower()
+print(re.findall(r'\bа\w+я\b', text))
